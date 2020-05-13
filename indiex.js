f627c525d3fb60682bc91b919100d15666057e5d
LourivalMartins <junior_tlx@live.com>
2020-05-13 14:46:05 -0300

    Valor '0'
    Input estiver vazio não inicia o código

--- indiex.js
+++ indiex.js
@@ -6,6 +6,17 @@ function clicar()
    var n2 = document.querySelector(".inp2").value;
    var n3 = document.querySelector(".inp3").value;
 
+   // Verifica se o valor dos inputs é diferente de '0'
+   // prog = progesso = 0 ou 1
+   
+   if (n1 == '' || n2 == '' || n3 == ''){
+         var prog = 0;
+      } else {
+         prog = 1;
+      }
+
+   if (prog == '1') {
+
    if (n2 == '+')
   { 
       var resultado = parseFloat(n1) + parseFloat(n3);
@@ -35,8 +46,15 @@ function clicar()
       document.querySelector(".resultado").innerHTML = 'R$ ' + resultado + ',00';
       document.querySelector(".dis").innerHTML = ' Operação divisão';
    }
-   // Ttransição da barra com o resultado
+   // transição da barra com o resultado
+
+   
+  
    document.querySelector(".barra").style.width = "130px";
+}
+else {
+   alert("Informe um valor válido");
+}
 }
 
       // Limpa todos os campos para valores iniciais
